036d684042076448f6f647d1e504798319a78492
achingbrain <alex@achingbrain.net>
2015-08-11 17:04:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# barbara-ngrok
+
+Open a tunnel to port 22 and dm me when done.  Also dm me if the tunnel goes down and is restablished.
+
+## Configuration
+
+* `TWITTER_RECIPIENT` Who to send the tweet to
+* `TWITTER_CONSUMER_KEY` Twitter credentials
+* `TWITTER_CONSUMER_SECRET` Twitter credentials
+* `TWITTER_ACCESS_TOKEN_KEY` Twitter credentials
+* `TWITTER_ACCESS_TOKEN_SECRET` Twitter credentials
+* `NGROK_PORT` Which port to forward (default 22)
+* `NGROK_PROTOCOL` Which protocol to use (default `tcp`)
---
+++ index.js
@@ -0,0 +1,36 @@
+var ngrok = require('@achingbrain/ngrok')
+var Twitter = require('twitter')
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
+function openTunnel () {
+  ngrok.connect({
+    proto: process.env.NGROK_PROTOCOL || 'tcp',
+    port: process.env.NGROK_PORT || 22
+  }, function (error, url) {
+    if (error) {
+      throw error
+    }
+
+    client.post('direct_messages/new', {
+      screen_name: process.env.TWITTER_RECIPIENT,
+      text: 'I am alive: ' + url
+    }, function (error) {
+      if (error) {
+        throw error
+      }
+    })
+  })
+}
+
+ngrok.on('close', function () {
+  // wait a little bit, then reopen tunnel
+  setTimeout(openTunnel, 1000)
+})
+
+openTunnel()
